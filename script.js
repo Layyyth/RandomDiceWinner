e83eb384f89bf59b06d392799d03bdca9d3d54b8
Layyyth <laithsocials@gmail.com>
2024-02-16 14:01:01 +0300

    Tie added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var DecidingFactor,num1,num2; 
-  
-    function setRandDiceImg() {
+  var DecidingFactor, num1, num2; 
+
+  function setRandDiceImg() {
       num1 = Math.floor(Math.random() * 6) + 1;
       num2 = Math.floor(Math.random() * 6) + 1;
   
@@ -12,19 +12,24 @@ document.addEventListener("DOMContentLoaded", function() {
       img2.src = "./images/dice" + num2 + ".png";
   
       
-      DecidingFactor = (num1 > num2) ? 1 : 0;
-    }
-  
-    function AnnounceWinner() {
-      var winner = document.querySelector(".container h1");
-      if (DecidingFactor) {
-        winner.innerHTML = " Player One Wins ";
+      if (num1 === num2) {
+          DecidingFactor = -1;
       } else {
-        winner.innerHTML = " Player Two Wins ";
+          DecidingFactor = (num1 > num2) ? 1 : 0;
       }
-    }
-  
-    setRandDiceImg();
-    AnnounceWinner();
-  });
-  
+  }
+
+  function AnnounceWinner() {
+      var winner = document.querySelector(".container h1");
+      if (DecidingFactor === -1) {
+          winner.innerHTML = "It's a Tie!";
+      } else if (DecidingFactor) {
+          winner.innerHTML = "Player One Wins";
+      } else {
+          winner.innerHTML = "Player Two Wins";
+      }
+  }
+
+  setRandDiceImg();
+  AnnounceWinner();
+});
