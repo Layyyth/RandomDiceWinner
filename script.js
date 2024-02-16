88d3c62056b4dc7d3fbba7381d51951c7b602ec3
Layyyth <laithsocials@gmail.com>
2024-02-16 13:49:59 +0300

    Project Done

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var DecidingFactor,num1,num2; 
+  
+    function setRandDiceImg() {
+      num1 = Math.floor(Math.random() * 6) + 1;
+      num2 = Math.floor(Math.random() * 6) + 1;
+  
+      var img1 = document.querySelector(".img1");
+      img1.src = "./images/dice" + num1 + ".png";
+  
+      var img2 = document.querySelector(".img2");
+      img2.src = "./images/dice" + num2 + ".png";
+  
+      
+      DecidingFactor = (num1 > num2) ? 1 : 0;
+    }
+  
+    function AnnounceWinner() {
+      var winner = document.querySelector(".container h1");
+      if (DecidingFactor) {
+        winner.innerHTML = " Player One Wins ";
+      } else {
+        winner.innerHTML = " Player Two Wins ";
+      }
+    }
+  
+    setRandDiceImg();
+    AnnounceWinner();
+  });
+  
